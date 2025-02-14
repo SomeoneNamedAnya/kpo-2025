ebe5d82e45d7e5a44c2c3e915e182c216e483437
SomeoneNamedAnya <76968547+SomeoneNamedAnya@users.noreply.github.com>
2025-02-14 14:50:00 +0300

    Delete Homeworks/HW1/src directory

--- Homeworks/HW1/src/main/java/homework/app/Inventory/Computer.java
+++
@@ -1,7 +0,0 @@
-package homework.app.Inventory;
-
-public class Computer extends Thing{
-    public Computer(int invNumber, String invName) {
-        super(invNumber, "Computer");
-    }
-}
--- Homeworks/HW1/src/main/java/homework/app/Inventory/Table.java
+++
@@ -1,7 +0,0 @@
-package homework.app.Inventory;
-
-public class Table extends Thing {
-    public Table(int invNumber, String invName) {
-        super(invNumber, "Table");
-    }
-}
